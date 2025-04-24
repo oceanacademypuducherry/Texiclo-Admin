d54034d22f858bd5a15af58a0e8c2ea67062702a
piemahesh <piemahesh@gmail.com>
2025-04-24 11:51:18 +0530

    #tailwind added successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
-export const App = () => {
-  return <div>App</div>;
-};
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-red-500">
+      <h1 className="text-4xl font-bold">Hi sangeetha</h1>
+    </div>
+  );
+}
