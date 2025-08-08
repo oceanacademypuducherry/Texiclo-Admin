5a9492ef52df2a1d250f2a2c50df958735aad5ae
rajeammu <raje0pa@gmail.com>
2025-08-08 13:07:00 +0530

    fix: deployment

--- src/services/API_Config.ts
+++ src/services/API_Config.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 export const AdminAPI = axios.create({
   baseURL:
-    "http://127.0.0.1:1001/oa-job-portal/asia-south1/texiclo_admin_test_api/app",
+    // "http://127.0.0.1:2001/texiclo-8b211/asia-south1/texiclo_admin_api/app",
+    "https://texiclo-admin-api-szkqgxi2aq-el.a.run.app/app",
 });
 
 AdminAPI.interceptors.request.use((config) => {
